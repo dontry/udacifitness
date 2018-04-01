cc6f5bf5830c56657e4daa28062565ae8d877884
dontry <mccoy018@gmail.com>
2018-04-01 16:08:19 +1000

    slider textButton

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,14 +1,35 @@
-import  React from 'react';
-import { View, Text, Slider } from 'react-native';
+import React from "react";
+import { View, Text, Slider, StyleSheet } from "react-native";
+import { gray } from "../utils/colors";
 
-export default function UdaciSlider({max, unit, step, value, onChange}) {
-    return (
-        <View>
-            <Slider step={step} value={value} maximumValue={max} minimumValue={0} onValueChange={onChange}/>
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
-            </View>
-        </View>
-    )
+export default function UdaciSlider({ max, unit, step, value, onChange }) {
+  return (
+    <View style={styles.row}>
+      <Slider
+      style={{flex: 1}} //take up the entire width of container
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View style={styles.metricCounter}>
+        <Text style={{textAlign: 'center'}}>{value}</Text>
+        <Text style={{color: gray}}>{unit}</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+ metricCounter: {
+    width: 85,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
