1d90a445ea274395f86b36ebe9b58641a0d6ef06
dontry <mccoy018@gmail.com>
2018-04-02 16:33:06 +1000

    location service 'granted' and 'denied'

--- components/Live.js
+++ components/Live.js
@@ -12,7 +12,7 @@ import { purple, white } from "../utils/colors";
 export default class Live extends Component {
   state = {
     coords: null,
-    status: "undetermined",
+    status: 'granted',
     direction: ""
   };
   askPermission = () => {};
@@ -25,8 +25,14 @@ export default class Live extends Component {
 
     if (status === "denied") {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.container}>
+          <View style={styles.center}>
+            <Foundation name="alert" size={50} />
+            <Text>
+              You denied your loaction. You can fix this by visiting your
+              settings and enable location services for this app.
+            </Text>
+          </View>
         </View>
       );
     }
@@ -51,34 +57,79 @@ export default class Live extends Component {
 
     return (
       <View style={styles.container}>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading </Text>
+          <Text style={styles.direction}>North</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Altitude</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Speed</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'space-between'
-    },
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 30,
-        marginRight: 30
-    },
-    button: {
-        padding: 10,
-        backgroundColor: purple,
-        alignSelf: 'center',
-        borderRadius: 5,
-        margin: 20
-    },
-    buttonText: {
-        color: white,
-        fontSize: 20
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 20
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center"
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: "center"
+  },
+  metricContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: purple
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "rgba(255,255,255,0.1)",
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 5
+  }
+});
