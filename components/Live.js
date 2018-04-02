20bfeea3e81238a514bd71a27f11d808ac75dad7
dontry <mccoy018@gmail.com>
2018-04-02 17:16:35 +1000

    geolocation

--- components/Live.js
+++ components/Live.js
@@ -8,14 +8,59 @@ import {
 } from "react-native";
 import Foundation from "@expo/vector-icons/Foundation";
 import { purple, white } from "../utils/colors";
+import { Location, Permissions } from "expo";
+import { calculateDirection } from "../utils/helpers";
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'granted',
+    status: "denied",
     direction: ""
   };
-  askPermission = () => {};
+  componentDidMount() {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+        this.setState(() => ({ status }));
+      })
+      .catch(error => {
+        console.warn("Error getting Location permission: ", error);
+        this.setState(() => ({ status: "undetermined" }));
+      });
+  }
+  askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+        this.setState(() => ({ status }));
+      })
+      .catch(error => {
+        console.warn("Error asking Location permission: ", error);
+      });
+  };
+  setLocation = () => {
+    Location.watchPosistionAsync(
+      {
+        enableHighAccuracy: true,
+        timeInterval: 1,
+        distanceInterval: 1
+      },
+      ({ coords }) => {
+        const newDirection = calculateDirection(coords.heading);
+        const { direction } = this.state;
+
+        this.setState(() => ({
+          coords,
+          status: "granted",
+          direction: newDirection
+        }));
+      }
+    );
+  };
   render() {
     const { status, coords, direction } = this.state;
 
@@ -40,7 +85,6 @@ export default class Live extends Component {
     if (status === "undetermined") {
       return (
         <View style={styles.container}>
-          <Text>undetermined</Text>
           <View style={styles.center}>
             <Foundation name="alert" size={50} />
             <Text>You need to enable location services for this app.</Text>
@@ -59,16 +103,20 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading </Text>
-          <Text style={styles.direction}>North</Text>
+          <Text style={styles.direction}>{direction}</Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Altitude</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {coords.altitude} Meters
+            </Text>
           </View>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Speed</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {(coords.speed * 2.2369).toFixed(1)} MPH
+            </Text>
           </View>
         </View>
       </View>
